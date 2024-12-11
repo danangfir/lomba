e2a3bdf0b6ded452d2a22185306cd604104aff04
NathanAbrahamSinaga <nathansinaga2002@gmail.com>
2024-12-11 23:56:39 +0700

    delete delete feature in stokin resource

--- app/Filament/Resources/StockinResource.php
+++ app/Filament/Resources/StockinResource.php
@@ -50,7 +50,6 @@ class StockinResource extends Resource
             ])
             ->actions([
                 // Tables\Actions\EditAction::make(),
-                Tables\Actions\DeleteAction::make(),
             ])
             ->bulkActions([
                 // Tables\Actions\BulkActionGroup::make([
