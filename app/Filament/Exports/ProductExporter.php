a4b5f1505466a8a83800f9105935761d5bf27c08
NathanAbrahamSinaga <nathansinaga2002@gmail.com>
2024-12-15 19:21:58 +0700

    change pr to product in ProductExporter file

--- app/Filament/Exports/ProductExporter.php
+++ app/Filament/Exports/ProductExporter.php
@@ -35,7 +35,7 @@ class ProductExporter extends Exporter
 
     public static function getCompletedNotificationBody(Export $export): string
     {
-        $body = 'Your pr export has completed and ' . number_format($export->successful_rows) . ' ' . str('row')->plural($export->successful_rows) . ' exported.';
+        $body = 'Your product export has completed and ' . number_format($export->successful_rows) . ' ' . str('row')->plural($export->successful_rows) . ' exported.';
 
         if ($failedRowsCount = $export->getFailedRowsCount()) {
             $body .= ' ' . number_format($failedRowsCount) . ' ' . str('row')->plural($failedRowsCount) . ' failed to export.';
